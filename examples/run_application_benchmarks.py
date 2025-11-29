10693ff299c8abdf398c6a731ea320f6e74a41b1
DonKamilo00 <maruszakkamil1@gmail.com>
2025-11-29 00:45:23 +0000

    updated benchmark

--- examples/run_application_benchmarks.py
+++ examples/run_application_benchmarks.py
@@ -6,8 +6,17 @@ import pandas as pd
 import scipy.special
 import matplotlib.pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
+import os
+import sys
 
 # --- IMPORTS ---
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..'))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+
 try:
     from fusion_kan import FusionKANLayer
     from kan.MultKAN import MultKAN
